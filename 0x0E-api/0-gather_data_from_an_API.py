5d1b42a7f1d5fed9849942b8e580743dfae58d77
NikkiMerena <alderman.nikki1022@gmail.com>
2023-03-31 13:49:10 -0700

    fixed pep8 error

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -8,16 +8,18 @@ import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
+def get_todo_progress(employee_id):
     """
     Fetches information about an employee's TODO list progress based on the given employee ID.
     """
     # Fetching the employee's details
-    response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    url_employee = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url_employee)
     employee_details = response.json()
 
     # Fetching the employee's todo list
-    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    url_todos = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    response = requests.get(url_todos)
     todo_list = response.json()
 
     # Calculating the employee's todo progress
@@ -26,12 +28,14 @@ def get_employee_todo_progress(employee_id):
 
     # Displaying the progress report
     employee_name = employee_details.get("name", "Unknown Employee")
-    print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+    progress_msg = f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):"
+    print(progress_msg)
     for task in todo_list:
         if task.get("completed"):
-            print(f"\t- {task['title']}")
+            task_title = f"\t- {task['title']}"
+            print(task_title)
 
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-    get_employee_todo_progress(employee_id)
+    get_todo_progress(employee_id)
